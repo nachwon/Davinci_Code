6795b583f00ab056de82b145d3d09df54ae091b9
Che1 <nachwon@gmail.com>
2019-08-31 02:51:08 +0900

    <refactor> Refactor main.py to create game object from the beginning

--- main.py
+++ main.py
@@ -15,41 +15,33 @@ async def feed(request, ws, *args, **kwargs):
     session_id = kwargs.get('session_id')
     manager = on_going_sessions.get(session_id)
     if not manager:
-        manager = GameManager()
+        manager = GameManager(session_id)
         on_going_sessions[session_id] = manager
     game = manager.game
 
     while True:
-        if not game:
+        message = await ws.recv()
+        message = MessageSerializer(message).deserialize()
+
+        if GameState(game.state) == GameState.CREATED:
             print('Waiting for players...')
-            message = await ws.recv()
-            m = MessageSerializer(message).deserialize()
-            if Actions(m.action) == Actions.ADD_PLAYER:
-                manager.add_player(name=m.body, ws=ws)
-                await ws.send(f'Player {m.body} have been added!')
-                print(manager._players)
+            if Actions(message.action) == Actions.ADD_PLAYER:
+                await manager.add_player(name=message.body, ws=ws)
+                # await ws.send(f'Player {message.body} have been added!')
+                # print(manager._players)
 
-            elif Actions(m.action) == Actions.START_GAME:
+            elif Actions(message.action) == Actions.START_GAME:
                 print('Game started!!')
-                game = manager.initiate_new_game(session_id)
-                await manager._distribute_message(message={
-                    "player_1": game.player_1.name,
-                    "player_2": game.player_2.name,
-                    "remaining_blocks": game.remaining_blocks
-                })
-        else:
-            if GameState(game.state) == GameState.INITIATED:
-                print('pick starting blocks!!')
-                message = await ws.recv()
-                message = MessageSerializer(message).deserialize()
-                await manager.update_game(message)
-                manager.check_ready()
+                await manager.initiate_new_game(session_id)
 
-            elif GameState(game.state) == GameState.PLAYING:
-                print('running!!')
-                message = await ws.recv()
-                message = MessageSerializer(message).deserialize()
-                await manager.update_game(message=message)
+        elif GameState(manager.game.state) == GameState.INITIATED:
+            print('pick starting blocks!!')
+            await manager.update_game(message)
+            manager.check_ready()
+
+        elif GameState(manager.game.state) == GameState.PLAYING:
+            print('running!!')
+            await manager.update_game(message=message)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, protocol=WebSocketProtocol)
