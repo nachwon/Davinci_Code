0a0f6cff0ef4f4271593507f13b984092efe8c06
Che1 <nachwon@gmail.com>
2019-09-05 20:19:35 +0900

    <feat> Add animation when hover on blocks

--- frontend/src/components/block.js
+++ frontend/src/components/block.js
@@ -1,50 +1,45 @@
 import React from 'react';
 import './block.css';
 
-
-class Block extends React.Component {
-    constructor(props) {
-        super(props)
-
-    }
-
-    render() {
-        const {index, number, color, showing} = this.props
-        console.log(showing)
-        return (
-            <div onClick={() => this.props.onClick(index)} className={`block-outer block-color-${color}`}>
-                <div className="block-inner">
-                    {
-                        showing ? 
-                            <div className="block-open">
-                                <div className="block-number">{number}</div>
-                                <div className="block-number-line" />
-                            </div>
-                            :
-                            <div className="block-close">
-                                <div className="block-close-dot-container">
-                                    <div className="block-close-dot-wrapper-right">
-                                        <div className="block-close-dot" />
-                                    </div>
-                                    <div className="block-close-dot-wrapper-center">
-                                        <div className="block-close-dot" />
-                                    </div>
-                                    <div className="block-close-dot-wrapper-left">
-                                        <div className="block-close-dot" />
-                                    </div>
-                                    <div className="block-close-dot-wrapper-center">
-                                        <div className="block-close-dot" />
-                                    </div>
-                                    <div className="block-close-dot-wrapper-right">
-                                        <div className="block-close-dot" />
-                                    </div>
+function Block(props) {
+    const {index, number, color, showing} = props
+    
+    return (
+        <div 
+            onClick={() => props.onClick(index)} 
+            className={`block-outer block-color-${color}`}
+        >
+            <div className="block-inner">
+                {
+                    showing ? 
+                        <div className="block-open">
+                            <div className="block-number">{number}</div>
+                            <div className="block-number-line" />
+                        </div>
+                        :
+                        <div className="block-close">
+                            <div className="block-close-dot-container">
+                                <div className="block-close-dot-wrapper-right">
+                                    <div className="block-close-dot" />
+                                </div>
+                                <div className="block-close-dot-wrapper-center">
+                                    <div className="block-close-dot" />
+                                </div>
+                                <div className="block-close-dot-wrapper-left">
+                                    <div className="block-close-dot" />
+                                </div>
+                                <div className="block-close-dot-wrapper-center">
+                                    <div className="block-close-dot" />
+                                </div>
+                                <div className="block-close-dot-wrapper-right">
+                                    <div className="block-close-dot" />
                                 </div>
                             </div>
-                    }
-                </div>
+                        </div>
+                }
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Block
